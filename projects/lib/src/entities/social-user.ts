4fdfdc3be7214cc814b66849f4560388e2ad650a
MartinDev <16200105+martindidonna@users.noreply.github.com>
2024-04-17 11:19:11 +0200

    Update social-user.ts

--- projects/lib/src/entities/social-user.ts
+++ projects/lib/src/entities/social-user.ts
@@ -1,15 +1,16 @@
 export class SocialUser {
-  provider: string;
-  id: string;
-  email: string;
-  name: string;
-  photoUrl: string;
-  firstName: string;
-  lastName: string;
-  authToken: string;
-
-  idToken: string; // Reference https://developers.google.com/identity/sign-in/web/backend-auth
-  authorizationCode: string; // Reference https://developers.google.com/identity/sign-in/web/reference#googleauthgrantofflineaccessoptions
-
-  response: any;
+    provider: string;
+    id?: string;
+    email: string;
+    name?: string;
+    photoUrl?: string;
+    firstName?: string;
+    lastName?: string;
+    authToken?: string;
+    idToken?: string;
+    authorizationCode?: string;
+    response?: any;
+    cookieTecnici?: boolean;
+    cookieAnalitici?: boolean;
+    cookieProfilazione?: boolean;
 }
